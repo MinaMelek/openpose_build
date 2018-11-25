56eb1933b2cedf7a42df9deefc961602a255b974
MinaMelek <mina.melek94@eng-st.cu.edu.eg>
2018-11-25 13:08:46 -0500

    second commit

---
+++ src/openpose/net/CMakeFiles/openpose_net.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/paperspace/openpose/src/openpose/net/bodyPartConnectorBase.cpp" "/home/paperspace/openpose/build/src/openpose/net/CMakeFiles/openpose_net.dir/bodyPartConnectorBase.cpp.o"
+  "/home/paperspace/openpose/src/openpose/net/bodyPartConnectorCaffe.cpp" "/home/paperspace/openpose/build/src/openpose/net/CMakeFiles/openpose_net.dir/bodyPartConnectorCaffe.cpp.o"
+  "/home/paperspace/openpose/src/openpose/net/maximumBase.cpp" "/home/paperspace/openpose/build/src/openpose/net/CMakeFiles/openpose_net.dir/maximumBase.cpp.o"
+  "/home/paperspace/openpose/src/openpose/net/maximumCaffe.cpp" "/home/paperspace/openpose/build/src/openpose/net/CMakeFiles/openpose_net.dir/maximumCaffe.cpp.o"
+  "/home/paperspace/openpose/src/openpose/net/netCaffe.cpp" "/home/paperspace/openpose/build/src/openpose/net/CMakeFiles/openpose_net.dir/netCaffe.cpp.o"
+  "/home/paperspace/openpose/src/openpose/net/netOpenCv.cpp" "/home/paperspace/openpose/build/src/openpose/net/CMakeFiles/openpose_net.dir/netOpenCv.cpp.o"
+  "/home/paperspace/openpose/src/openpose/net/nmsBase.cpp" "/home/paperspace/openpose/build/src/openpose/net/CMakeFiles/openpose_net.dir/nmsBase.cpp.o"
+  "/home/paperspace/openpose/src/openpose/net/nmsBaseCL.cpp" "/home/paperspace/openpose/build/src/openpose/net/CMakeFiles/openpose_net.dir/nmsBaseCL.cpp.o"
+  "/home/paperspace/openpose/src/openpose/net/nmsCaffe.cpp" "/home/paperspace/openpose/build/src/openpose/net/CMakeFiles/openpose_net.dir/nmsCaffe.cpp.o"
+  "/home/paperspace/openpose/src/openpose/net/resizeAndMergeBase.cpp" "/home/paperspace/openpose/build/src/openpose/net/CMakeFiles/openpose_net.dir/resizeAndMergeBase.cpp.o"
+  "/home/paperspace/openpose/src/openpose/net/resizeAndMergeBaseCL.cpp" "/home/paperspace/openpose/build/src/openpose/net/CMakeFiles/openpose_net.dir/resizeAndMergeBaseCL.cpp.o"
+  "/home/paperspace/openpose/src/openpose/net/resizeAndMergeCaffe.cpp" "/home/paperspace/openpose/build/src/openpose/net/CMakeFiles/openpose_net.dir/resizeAndMergeCaffe.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_CAFFE"
+  "USE_CUDA"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "../include"
+  "caffe/include"
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/paperspace/openpose/build/src/openpose/core/CMakeFiles/openpose_core.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
